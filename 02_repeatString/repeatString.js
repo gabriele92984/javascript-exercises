7a72f3bc65a5fb981669bc71437b5fdcf1f745ea
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-07-07 15:36:51 +0200

    Add loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  
+  let outString;
+  let i = num;
+  while (i) {
+    outString += string;
+    i--;
+  }
+
+  return outString;
 
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
