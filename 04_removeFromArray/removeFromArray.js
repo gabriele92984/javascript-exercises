9bf2fa746622b167438faa95cde8338f94575bef
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-07-08 12:43:51 +0200

    Rewrite removeFromArray() using Array.prototype.filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,4 @@
-/*const removeFromArray = function() {
-};*/
-
+/*
 const removeFromArray = function(firstArray,...toRemove) {
   let modifiedArray = [...firstArray];
   for (let i = 0; i < toRemove.length; i++) {
@@ -9,6 +7,16 @@ const removeFromArray = function(firstArray,...toRemove) {
       }    
   }
   return modifiedArray;
+};
+*/
+
+/* This function uses the Array.prototype.filter method to create 
+a new array that only contains elements not included in the itemsToRemove array. 
+The spread operator (...) is used to allow for any number of optional arguments to 
+be passed to the function. */
+
+const removeFromArray = function(arr, ...itemsToRemove) {
+  return arr.filter(item => !itemsToRemove.includes(item));
 };
 
 removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
