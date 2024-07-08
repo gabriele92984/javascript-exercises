414a207ec2b940d65a9e9a4711cc7deab6ce2376
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-07-08 12:20:47 +0200

    Write removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/*const removeFromArray = function() {
+};*/
+
+const removeFromArray = function(firstArray,...toRemove) {
+  let modifiedArray = [...firstArray];
+  for (let i = 0; i < toRemove.length; i++) {
+      if (modifiedArray.includes(toRemove[i])) {
+          modifiedArray.splice(modifiedArray.indexOf(toRemove[i]), 1)
+      }    
+  }
+  return modifiedArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
