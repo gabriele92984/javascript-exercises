b31cca9f07ee180ff666558f01fe6b34273b533c
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-07-10 11:30:55 +0200

    Pass all tests palindromes.js exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
 const palindromes = function (string) {
-    return string === string.split("").reverse().join("");
+  let original = string.replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g,"").toLowerCase();
+
+//take original sentence and reverse
+  let reverse = original.split('').reverse().join('');
+
+//compare original vs reversed
+  if (original === reverse) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
