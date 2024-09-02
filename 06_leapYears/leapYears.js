8f33713c2c8ad8bae7d3eb4a8f48d7ef0b47a3ce
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-02 14:07:58 +0200

    Refactor exercise 60_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,31 @@
 const leapYears = function(year) {
-  if (year % 400 === 0) {
-      return true;
-  } else if (year % 100 === 0) {
-      return false;
-  } else if (year % 4 === 0) {
-      return true;
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true; // Year divisible by 400
+      } else {
+        return false; // Year divisible by 100 but not by 400
+      }
+    } else {
+      return true; // Year divisible by 4 but not by 100
+    }
   } else {
-      return false;
+    return false; // Year is not divisible by 4
   }
 };
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+/* 
+if (year % 400 === 0) {
+  return true;
+} else if (year % 100 === 0) {
+  return false;
+} else if (year % 4 === 0) {
+  return true;
+} else {
+  return false;
+}  
+*/
